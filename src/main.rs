416ad30ebbdf2d214ee4934e01f79048bb49497b
sMayank21 <albertnike1371@gmail.com>
2025-08-14 10:40:38 +0530

    threads added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use rayon::prelude::*;
+
+fn main() -> Result<()> {
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(256)
+        .build()
+        .unwrap();
+
+    pool.install(|| {
+        let scan_result: Vec<Subdomain> = subdomain::enumerate(http_client)
+            .unwrap()
+            .into_par_iter()
+            .map(ports::scan_ports)
+            .collect();
+
+        for subdomain in scan_result {
+            println!("{}:",&subdomain.domain);
+            for port in &subdomain.open_ports {
+                println!("  {}",port.port);
+            }
+            println("");
+        }
+    });
 }
--- src/ports.rs
+++ src/ports.rs
@@ -8,7 +8,7 @@ use::rayon::prelude::*;
 
 pub fn scan_ports(mut subdomain: Subdomain) -> Subdomain {
     subdomain.open_ports = MOST_COMMON_PORTS_10
-        .into_iter()
+        .into_par_iter()
         .map(|port| scan_port(&subdomain.domain, *port))
         .filter(|port| port.is_open)
         .collect();
